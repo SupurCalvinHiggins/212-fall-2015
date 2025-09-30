c27a69583a5b6613c451516676ca8f9a17133ad4
Marco <13228776+MarcoAlvarez@users.noreply.github.com>
2025-09-30 16:42:21 -0400

    Add files via upload

--- assignments/ha-03/src/stack.h
+++ assignments/ha-03/src/stack.h
@@ -1,1 +1,19 @@
+#pragma once
+#include <cstddef>
 
+class Stack {
+    private:
+        int* data;
+        size_t capacity;
+        size_t n_elem;
+
+    public:
+        Stack(size_t cap);
+        ~Stack();
+
+        void push(int val);
+        void pop();
+        int top() const;
+        bool empty() const;
+        size_t size() const;
+};
